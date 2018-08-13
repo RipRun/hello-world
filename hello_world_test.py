5363a04682d6acb1fd29e5b29f5718b974a38153
erinripple <ripple.erin@comcast.net>
2018-08-12 19:44:08 -0700

    Intro to Pycharm examples

--- hello_world_test.py
+++ hello_world_test.py
@@ -1,5 +1,36 @@
 def main():
     print("Hello world")
+    name = input("What is your name? ")
+    print("Hello {}!".format(name))
+    x = 10
+    print(x)
+    number = int(input("Give me a number: "))
+    if number > 10:
+        print("{} is greater than 10".format(str(number)))
+    elif number < 10:
+        print("{} is less than 10".format(str(number)))
+    else:
+        print("{} is equal to 10".format(str(number)))
+    my_list = [1, 2, 3, 4, 5]
+    for e in my_list:
+        print(e)
+    import random
+    lister = []
+    for i in range(10):
+        lister.append(random.randint(1, 100))
+    num = int(input("Enter a number: "))
+    count = 0
+    for e in lister:
+        if e < num:
+            count += 1
+    print(str(count))
+
+    def take_average():
+        print("average")
+    take_average()
+    z = "stop"
+    if z == "stop":
+        print("ok")
 
 
 if __name__ == "__main__":
